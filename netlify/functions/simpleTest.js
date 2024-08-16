ace63132961d4f692ea97d766b0ade8f412e6cbe
Abr <59525997+Abr009@users.noreply.github.com>
2024-08-16 20:01:41 +0530

    Update simpleTest.js

--- netlify/functions/simpleTest.js
+++ netlify/functions/simpleTest.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = require('./keys/serviceAccountKey.json');
 
 // Initialize Firebase
 admin.initializeApp({
