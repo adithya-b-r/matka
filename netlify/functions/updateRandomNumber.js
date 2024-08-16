5144ceef3b3d12a0a23f56562b94df5821134f9b
Abr <59525997+Abr009@users.noreply.github.com>
2024-08-16 14:52:01 +0530

    Update updateRandomNumber.js

--- netlify/functions/updateRandomNumber.js
+++ netlify/functions/updateRandomNumber.js
@@ -1,29 +1,31 @@
-// netlify/functions/updateRandomNumber.js
 const admin = require('firebase-admin');
-
-// Initialize Firebase Admin SDK
-const serviceAccount = require('./keys/serviceAccountKey.json'); // Path to your service account key
+const serviceAccount = require('./keys/serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://matka-78bf1-default-rtdb.europe-west1.firebasedatabase.app"
+  databaseURL: 'https://matka-78bf1-default-rtdb.europe-west1.firebasedatabase.app'
 });
 
 const db = admin.database();
 
 exports.handler = async function(event, context) {
-  const randomNumber = Math.floor(Math.random() * 100); // Generate random number between 0 and 99
-
   try {
-    await db.ref('randomNumber').set(randomNumber); // Update the random number in Firebase
+    const number = Math.floor(Math.random() * 100);
+    const ref = db.ref('randomNumbers');
+    await ref.set({ number });
+
+    console.log(`Number updated to: ${number}`);
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Random number ${randomNumber} written to database.` }),
+      body: JSON.stringify({ message: 'Number updated successfully' })
     };
   } catch (error) {
+    console.error('Error updating number:', error);
+
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: error.message }),
+      body: JSON.stringify({ message: 'Error updating number' })
     };
   }
 };
